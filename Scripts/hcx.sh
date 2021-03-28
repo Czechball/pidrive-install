198459b117e9cb1052e84342532b1502d6f2a23c
Czechball <david@jenne.cz>
2021-03-28 06:23:28 +0200

    fixed interface name and improved channel coverage

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -2,7 +2,7 @@
 if (iw dev | grep wlan0); then
 	while true; do
 		if (iw dev | grep wlan0); then
-			hcxdumptool --error_max=1 -i wlan0 -o "/opt/wardriving/$HOSTNAME-capture.pcapng" --disable_client_attacks --enable_status=95 >> /var/log/hcxdumptool.log
+			hcxdumptool --error_max=1 -i wlan1 -s 1 -o "/opt/wardriving/$HOSTNAME-capture.pcapng" --disable_client_attacks --enable_status=95 >> /var/log/hcxdumptool.log
 			echo "hcxdumptool stopped" >> /var/log/hcxdumptool.log
 			echo "hcxdumptool stopped, waiting for 5 seconds..."
 			sleep 5
