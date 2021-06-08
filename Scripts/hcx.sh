a6b2470a71c35cc96b5c1451d16c1f72bd31726e
Czechball <david@jenne.cz>
2021-06-08 18:39:15 +0200

    updated wardriving script

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-INTERFACE="wlan0"
 DIRECTORY="/opt/wardriving"
-LOGFILE="/var/log/hcxdumptool.log"
+INTERFACE="$1"
 
-if (iw dev | grep "$INTERFACE"); then
-	while true; do
-		if (iw dev | grep "$INTERFACE"); then
-			hcxdumptool --error_max=1 -i "$INTERFACE" -s 1 -o "$DIRECTORY/$HOSTNAME-capture.pcapng" --disable_client_attacks --enable_status=95 >> "$LOGFILE"
-			echo "hcxdumptool stopped" >> "$LOGFILE"
-			echo "hcxdumptool stopped, waiting for 5 seconds..."
-			sleep 5
-		fi
-	done
+if [[ $(iw dev | grep "$INTERFACE") != "" ]]; then
+        while true; do
+                if (iw dev | grep "$INTERFACE"); then
+                        hcxdumptool --error_max=1 -i "$INTERFACE" -o "$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng" --disable_client_attacks --enable_status=95 >> /var/log/hcxdumptool-"$INTERFACE".log
+                        echo "hcxdumptool stopped" >> /var/log/hcxdumptool.log
+                        echo "hcxdumptool stopped, waiting for 5 seconds..."
+                        sleep 5
+                        exit
+                fi
+        done
 else
-	sleep 5
-	exit
+        sleep 5
+        exit
 fi
