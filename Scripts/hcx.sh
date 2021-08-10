cb128dfb6ad33097e99649460b1d8dc30e50a24d
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-10 20:48:15 +0200

    simplified hcx service

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -3,17 +3,8 @@
 DIRECTORY="/opt/wardriving"
 INTERFACE="$1"
 
-if [[ $(iw dev | grep "$INTERFACE") != "" ]]; then
-        while true; do
-                if (iw dev | grep "$INTERFACE"); then
-                        hcxdumptool --error_max=1 -i "$INTERFACE" -o "$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng" --disable_client_attacks --enable_status=95 >> /var/log/hcxdumptool-"$INTERFACE".log
-                        echo "hcxdumptool stopped" >> /var/log/hcxdumptool.log
-                        echo "hcxdumptool stopped, waiting for 5 seconds..."
-                        sleep 5
-                        exit
-                fi
-        done
-else
-        sleep 5
-        exit
+if (iw dev | grep "$INTERFACE"); then
+        hcxdumptool --error_max=1 -i "$INTERFACE" -o "$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng" --disable_client_attacks --enable_status=95 >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
+        echo "hcxdumptool stopped" >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
+        echo "hcxdumptool stopped, waiting for 5 seconds..."
 fi
