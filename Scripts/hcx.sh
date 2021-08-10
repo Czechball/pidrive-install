3316781c2feee424b0ede56a8ebf1ca024822656
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-10 20:49:33 +0200

    removed message about waiting

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -6,5 +6,4 @@ INTERFACE="$1"
 if (iw dev | grep "$INTERFACE"); then
         hcxdumptool --error_max=1 -i "$INTERFACE" -o "$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng" --disable_client_attacks --enable_status=95 >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
         echo "hcxdumptool stopped" >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
-        echo "hcxdumptool stopped, waiting for 5 seconds..."
 fi
