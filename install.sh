4377bae6792510d3257a6bb403ff4a4d030da4f9
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-16 02:16:44 +0200

    removed post-reboot code

--- install.sh
+++ install.sh
@@ -35,39 +35,9 @@ mount -o remount,rw /boot
 apt --reinstall install raspberrypi-kernel raspberrypi-kernel-headers -y
 rm -rf /lib/modules/*/build
 apt --reinstall install raspberrypi-kernel-headers -y
-#reboot
+
+echo "To finish the kernel-headers fix, please reboot"
 
 ##
 #idk if this works, needs testing
 #this needs to be moved into another script to be executed after reboot after kernel "repair"
-
-# Clone and install hcxdumptool
-
-sudo -u pi bash -c 'cd ~;git clone https://github.com/ZerBea/hcxdumptool.git;cd hcxdumptool;make;sudo make install'
-
-# Clone and install hcxtools
-
-sudo -u pi bash -c 'cd ~;git clone https://github.com/ZerBea/hcxtools.git;cd hcxtools;sudo make install'
-
-# Clone and install realtek_rtwifi
-# Blacklist 8188eu, r8188eu and rtl8xxxu
-
-printf "blacklist 8188eu\nblacklist r8188eu\nblacklist rtl8xxxu" > /etc/modprobe.d/realtek.conf
-sudo -u pi bash -c 'cd ~;git clone https://github.com/kimocoder/realtek_rtwifi;cd realtek_wifi;make;sudo make install'
-
-##
-
-cp Scripts/* /usr/bin/
-
-# Create wardriving directory
-
-mkdir -p /opt/wardriving/logs
-
-# Copy services
-
-cp Services/* /etc/systemd/system/
-systemctl daemon-reload
-
-# Disable services taking over wireless interfaces
-
-systemctl --now disable avahi-daemon
