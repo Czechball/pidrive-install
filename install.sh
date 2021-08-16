67718661b3f238cbd5636cf3f123dd568317b2b8
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-16 03:28:28 +0200

    added some testing

--- install.sh
+++ install.sh
@@ -10,6 +10,16 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+echo "Warning: This script will only work on Raspberry Pi OS and possibly other Debian based systems."
+
+# Check internet connectivity
+
+if (curl -s google.com > /dev/null); then
+   :
+else
+   echo "Error, can't reach Google. Make sure you have internet connectivity before running this script."
+fi
+
 # Update and upgrade the system, install dependencies
 
 echo "Running apt update and apt upgrade, this will take a while"
@@ -22,11 +32,19 @@ apt install -y $PACKAGES
 
 # Fix mt7610u drivers, see https://github.com/raspberrypi/firmware/issues/1563
 
-sudo -u pi bash -c 'wget http://ftp.uk.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-misc-nonfree_20190114-2_all.deb'
-dpkg -i firmware-misc-nonfree_20190114-2_all.deb
+echo "Checking if the correct version (1:20190114-2+rpt1) of firmware-misc-nonfree is installed... "
+
+if (apt list firmware-misc-nonfree | grep 20190114-2); then
+   echo "Success, skipping reinstall"
+else
+   echo "Correct version not found, installing the correct one..."
+   sudo -u pi bash -c 'wget http://ftp.uk.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-misc-nonfree_20190114-2_all.deb'
+   dpkg -i firmware-misc-nonfree_20190114-2_all.deb
+fi
+
 apt-mark hold firmware-misc-nonfree
 
-echo "Reinstalling kernel headers so we can compile drivers..."
+echo "Reinstalling kernel headers so we can compile drivers... (this might take a while)"
 
 # Remount /boot as read-write
 
@@ -37,7 +55,7 @@ apt --reinstall install raspberrypi-kernel raspberrypi-kernel-headers -y
 rm -rf /lib/modules/*/build
 apt --reinstall install raspberrypi-kernel-headers -y
 
-echo "To finish the kernel-headers fix, please reboot"
+echo "To finish the kernel-headers fix, please reboot. Then run $SCRIPTPATH/finalize.sh"
 
 ##
 #idk if this works, needs testing
