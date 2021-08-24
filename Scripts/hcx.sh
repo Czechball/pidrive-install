8f442a3d7767435d13a18e834d119dd761e79e8b
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-25 00:59:14 +0200

    fixed command variable

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -2,16 +2,15 @@
 
 DIRECTORY="/opt/wardriving"
 INTERFACE="$1"
-WARDRIVE_COMMAND="hcxdumptool --error_max=1 -i \"$INTERFACE\" -o \"$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng\" --disable_client_attacks --enable_status=95"
 
 wardrive()
 {
         HCX_OPTS=$(grep "$INTERFACE" /opt/wardriving/interfaces.txt | cut -d ";" -f 3)
         if [[ $HCX_OPTS == "" ]]; then
-                $WARDRIVE_COMMAND
+                hcxdumptool --error_max=1 -i $INTERFACE -o $DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng --disable_client_attacks --enable_status=95
 
         else
-                $WARDRIVE_COMMAND $HCX_OPTS
+                hcxdumptool --error_max=1 -i $INTERFACE -o $DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng --disable_client_attacks --enable_status=95 $HCX_OPTS
         fi
 }
 
