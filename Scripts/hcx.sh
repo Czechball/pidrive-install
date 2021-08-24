79d7e00028960c258523f5e8ebf384fc0609eab0
Czechball <13371865+Czechball@users.noreply.github.com>
2021-08-25 00:39:59 +0200

    added options parsing from interfaces.txt

--- Scripts/hcx.sh
+++ Scripts/hcx.sh
@@ -2,8 +2,20 @@
 
 DIRECTORY="/opt/wardriving"
 INTERFACE="$1"
+WARDRIVE_COMMAND="hcxdumptool --error_max=1 -i \"$INTERFACE\" -o \"$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng\" --disable_client_attacks --enable_status=95"
+
+wardrive()
+{
+        HCX_OPTS=$(grep "$INTERFACE" /opt/wardriving/interfaces.txt | cut -d ";" -f 3)
+        if [[ $HCX_OPTS == "" ]]; then
+                $WARDRIVE_COMMAND
+
+        else
+                $WARDRIVE_COMMAND $HCX_OPTS
+        fi
+}
 
 if (iw dev | grep "$INTERFACE"); then
-        hcxdumptool --error_max=1 -i "$INTERFACE" -o "$DIRECTORY/$HOSTNAME-$INTERFACE-capture.pcapng" --disable_client_attacks --enable_status=95 >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
+        wardrive >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
         echo "hcxdumptool stopped" >> "$DIRECTORY"/logs/hcxdumptool-"$INTERFACE".log
 fi
