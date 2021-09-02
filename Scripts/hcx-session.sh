18075e1f415612aa729143c5dbc9bdcfaaebdd73
Czechball <13371865+Czechball@users.noreply.github.com>
2021-09-02 11:27:30 +0200

    improved session system, trailing zeros

--- Scripts/hcx-session.sh
+++ Scripts/hcx-session.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rm /opt/wardriving/session.lock
+if test -f "/opt/wardriving/sessions.txt"; then
+	LAST_SESSION=$(tail -n1 "/opt/wardriving/sessions.txt")
+	CURRENT_SESSION=$((LAST_SESSION + 1))
+	CURRENT_SESSION_WZERO=$(printf %04d%s ${CURRENT_SESSION%.*} ${i##*.})
+	echo $CURRENT_SESSION_WZERO >> "/opt/wardriving/sessions.txt"
+else
+	echo "0000" >> "/opt/wardriving/sessions.txt"
+fi
