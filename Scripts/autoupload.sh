55a816b16aa8506c1303f510d8674cd54a120781
Czechball <13371865+Czechball@users.noreply.github.com>
2022-09-28 14:53:54 +0200

    Initial commit

---
+++ Scripts/autoupload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+touch ~/.uploaded
+WARDRIVE_DIR="/opt/wardriving/"
+
+loop()
+{
+        sleep 10m
+        upload
+}
+
+upload()
+{
+        while read LINE; do
+                if grep -Fxq "$LINE" ~/.uploaded; then
+                        echo "Session $LINE was already uploaded, skipping.."
+                else
+                        echo "Session $LINE not uploaded yet, uploading.."
+                        upload-pcapng.sh "$WARDRIVE_DIR""$LINE"/* || retry
+                        echo "Session $LINE uploaded succesfully"
+                        echo "$LINE" >> ~/.uploaded
+                fi
+        done </opt/wardriving/sessions.txt
+        loop
+}
+
+retry()
+{
+        echo "Upload failed, waiting 10m and trying again"
+        sleep 10m
+        upload
+}
+
+upload
