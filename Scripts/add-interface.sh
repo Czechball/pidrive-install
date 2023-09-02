38683a19befbede5be6429d05343308d93ab646e
N3ttX <n3ttx0@gmail.com>
2023-09-02 13:55:43 +0200

    Fixed non-working interface list
    Hcxdumptool removed support for listing interfaces normally. Thanks to ZerBea, grepable single output was added back, so this re-introduces that and fixes non-working script

--- Scripts/add-interface.sh
+++ Scripts/add-interface.sh
@@ -14,7 +14,7 @@ read_interface()
    INTERFACE_NAME=${INTERFACE_NAME:-$INTERFACE}
 }
 
-INTERFACES=$(hcxdumptool -I | tail -n +2 | cut -f 3)
+INTERFACES=$(hcxdumptool -l | awk 'BEGIN { FS = "\t" } ; { print $6 }')
 INTERFACES=($INTERFACES)
 
 for ITEM in ${INTERFACES[@]}; do
